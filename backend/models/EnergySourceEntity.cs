8553e9435c637bf6d6b9e53e9d95a3466fb57f16
Mircea Rilă <mircea.rila@gmail.com>
2022-06-18 15:02:00 +0300

    temporary remove references to azure storage

--- backend/models/EnergySourceEntity.cs
+++ backend/models/EnergySourceEntity.cs
@@ -1,15 +1,13 @@
 using System;
-using Azure;
-using Azure.Data.Tables;
 
 namespace Monocube.RoEnergy.Models;
 
-public sealed class EnergySourceEntity : ITableEntity
+public sealed class EnergySourceEntity
 {
     public string PartitionKey { get; set; }
     public string RowKey { get; set; }
     public DateTimeOffset? Timestamp { get; set; }
-    public ETag ETag { get; set; }
+    // public ETag ETag { get; set; }
     public string Name { get; set; }
     public float Capacity { get; set; }
 }
